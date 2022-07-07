ea37e24c1e6c4857dccc1dbf9490cbe15868ba79
YagiAyana <cgjd0310@mail4.doshisha.ac.jp>
2022-07-08 04:49:59 +0900

    ホーム画面

--- front/src/coomponents/parts/button.js
+++ front/src/coomponents/parts/button.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// pathとnameが引数として親から渡されている
+
 function NextButton({path,name}) {
     return(
-        // Linkのto属性にpathを渡す
-        <Link to={`${path}`} className="font-semibold rounded shadow-lg text-4xl bg-purple hover:bg-thick-purple text-white mt-44 font-bold py-4 px-20 rounded-full inline-block hover:shadow-sm hover:translate-y-0.5 transform transition">
+        <Link to={`${path}`} className="font-semibold rounded shadow-lg text-4xl bg-purple hover:bg-thick-purple text-white font-bold py-4 px-20 rounded-full inline-block hover:shadow-sm hover:translate-y-0.5 transform transition">
             {name}
-            {/* ブラウザに表示させる文字としてnameを渡す */}
         </Link>
     );
 }
