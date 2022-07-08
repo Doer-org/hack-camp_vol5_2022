78fc34df236f529809971fa3df6d427855faee4f
mahiro72 <mahiro.yoshie@gmail.com>
2022-07-09 02:36:48 +0900

    fin: websocket!

--- front/src/coomponents/views/UserList.js
+++ front/src/coomponents/views/UserList.js
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from "react";
 import Socket from "../../ws/socket";
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import User from "../parts/User";
 
 const UserList = () => {  
   
@@ -9,10 +10,15 @@ const UserList = () => {
   const query = new URLSearchParams(search);
   const room = query.get('room');
 
+  // websocket接続準備
   const ws = new WebSocket(`ws://localhost:8080/ws?room=${room}`);
   const socket = new Socket(ws);
 
   const [userList, setUserList] = useState([]);
+
+  const [roomName, setRoomName] = useState("");
+  const [maxCount, setMaxCount] = useState(0);
+  const [nowCount, setNowCount] = useState(0);
 
   useEffect(() => {
       // connect, disconnect, message eventの追加
@@ -20,9 +26,12 @@ const UserList = () => {
       socket.on("disconnect",onDisConnect);
       socket.on("message",receiveMessage);
 
+      // 初回のmemberアクセス
       axios
         .get(`http://localhost:8080/member/all?room=${room}`)
-        .then((data)=>{console.log("member: ",data)})
+        .then((res)=>{
+          setUserList(res.data.data)
+        })
         .catch((err)=>{
           console.log(err)
         })
@@ -30,6 +39,7 @@ const UserList = () => {
 
   const onConnect = ()=>{
     console.log("ws connect")
+    ws.send("connect")
   };
 
   const onDisConnect = ()=>{
@@ -37,23 +47,84 @@ const UserList = () => {
   };
 
   const receiveMessage = (data)=>{
+    // websocketで通信を受け取るたびにmember更新
+    // websocketもどき
     if (data){
       //get member
       console.log("receive data",data)
+      axios
+        .get(`http://localhost:8080/member/all?room=${room}`)
+        .then((res)=>{
+          setUserList(res.data.data)
+          setNowCount(res.data.data.length)
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
 
+      axios
+        .get(`http://localhost:8080/room/${room}`)
+        .then((res)=>{
+          setRoomName(res.data.data.name)
+          setMaxCount(res.data.data.max_count)
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
     }
   };
 
-  const wsfunc = ()=>{
-    socket.send("hello")
-  }
-
   return (
-    <div>
-      <div>ユーザー一覧</div>
-      <button className="bg-blue-500 hover:bg-blue-400 text-white rounded px-4 py-2" onClick={wsfunc}>Send</button>
-      
-    </div>
+    <>
+      <div className="py-10 h-screen bg-gray-300 px-2">
+          <div className="max-w-md mx-auto bg-gray-100 shadow-lg rounded-lg overflow-hidden md:max-w-lg">
+              <div className="md:flex">
+                  <div className="w-full p-4">
+                    {
+                      maxCount!==nowCount ? 
+                      <div className="">
+                        <p className="flex justify-center p-2 text-gray-800">Room <span className="text-2xl mx-4">{roomName}</span></p>
+
+                        <p className="flex justify-center">他のmemberを待っています...</p>
+                        <p className="flex justify-center">現在 {nowCount} / {maxCount}</p>
+                        <div className="flex justify-center">
+                          <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+                        </div>
+                      </div>
+                
+                      :
+                      <>
+                        <h1>準備完了</h1>
+                        {roomName}
+                      </>
+                    }
+                      <ul>
+                          {
+                            userList.map((user, idx)=>{
+                                return <User user={user} key={idx} />
+                              })
+                          }
+                      </ul>
+                      {
+                        maxCount!==nowCount ? 
+                        <>
+                        </>
+                  
+                        :
+                        <>
+                          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Button
+                          </button>
+                        </>
+                      }
+                  </div>
+
+              </div>
+          </div>
+      </div>
+    </>
+    
+
   );
 };
 export default UserList;
