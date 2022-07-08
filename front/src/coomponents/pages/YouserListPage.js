df101e04691acbc07370ba0da93eca0c4a19b101
Kouta-fd <kotafd0124@icloud.com>
2022-07-08 22:37:49 +0900

    feat:ユーザー一覧ページ追加

---
+++ front/src/coomponents/pages/YouserListPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import YouserList from "../views/YouserList";
+
+export class YouserListPage extends React.Component {
+  render() {
+    return <YouserList />;
+  }
+}
---
+++ front/src/coomponents/views/YouserList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const YouserList = () => {
+  return (
+    <div>
+      <div>ユーザー一覧</div>
+    </div>
+  );
+};
+export default YouserList;
