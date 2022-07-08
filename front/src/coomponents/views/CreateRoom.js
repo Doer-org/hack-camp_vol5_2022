cc3f20141ff6012390fc140e2288807daf761575
YagiAyana <cgjd0310@mail4.doshisha.ac.jp>
2022-07-08 16:12:56 +0900

    「ルーム作成文字」追加

--- front/src/coomponents/views/CreateRoom.js
+++ front/src/coomponents/views/CreateRoom.js
@@ -4,6 +4,10 @@ export default class CreateRoom extends React.Component {
   render() {
     return (
       <div>
+        <div align="center">
+          <h1 className="text-5xl mt-28">ルーム作成</h1>
+        </div>
+        
         CreateRoom
       </div>
     )
