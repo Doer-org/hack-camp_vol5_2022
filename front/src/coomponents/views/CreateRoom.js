3a9a120683801a602966c4cb5ca408f09d76b0aa
YagiAyana <cgjd0310@mail4.doshisha.ac.jp>
2022-07-09 01:20:39 +0900

    人数選択ボタンを制作

--- front/src/coomponents/views/CreateRoom.js
+++ front/src/coomponents/views/CreateRoom.js
@@ -1,23 +1,31 @@
-import React from "react";
-import Select from 'react-select';
+import {React, useState} from "react";
+// import Select from 'react-select';
 
 const CreateRoom = () =>  {
-  const options = [
-    { value: 'pikachu', label: 'ピカチュウ' },
-    { value: 'bulbasaur', label: 'フシギダネ' },
-    { value: 'charmander', label: 'ヒトカゲ' },
-    { value: 'squirtle', label: 'ゼニガメ' },
-  ]
-    return (
-      <div>
-        <div align="center">
-          <h1 className="text-5xl mt-28">ルーム作成</h1>
-        </div>
+  const [count, setCount] = useState(0);
+  
+  const CountUp = () => {
+    setCount(count+1)
+  }
 
-        <div className="w-72" align="center">
-        <Select options={options} label="人数"/>
-        </div> 
+  const CountDown = () => {
+    if (count>0){
+      setCount(count-1);
+    }
+  }
+
+  return (
+    <div>
+      <div align="center">
+        <h1 className="text-5xl mt-28">ルーム作成</h1>
       </div>
+      <div align="center" className="text-4xl mt-24">人数</div>
+      <div align="center" className="text-5xl mt-10 flex justify-center">
+        <button onClick={CountDown} className="flex-grow-0 bg-grey text-white w-12 h-12 text-lg shadow-lg rounded-full mr-4 hover:bg-thick-grey hover:shadow-sm hover:translate-y-0.5 transform transition">ー</button>
+        <div className="flex-grow-0 px-20 py-1 bg-purple text-lg text-white rounded-full">{count}</div>
+        <button onClick={CountUp} className="flex-grow-0 bg-grey text-white w-12 h-12 text-lg shadow-lg rounded-full ml-4 hover:bg-thick-grey hover:shadow-sm hover:translate-y-0.5 transform transition">＋</button>
+      </div>
+    </div>
     );
 }
 
