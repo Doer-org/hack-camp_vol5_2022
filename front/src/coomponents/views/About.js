af62194a832e329797a5f84bba813e1cb5554b41
YagiAyana <cgjd0310@mail4.doshisha.ac.jp>
2022-07-10 00:48:23 +0900

    Aboutページ途中まで作成

--- front/src/coomponents/views/About.js
+++ front/src/coomponents/views/About.js
@@ -5,7 +5,30 @@ const About = () => {
         <div> 
             <div align="center">
                 <h1 className="text-6xl mt-24">タイトル</h1>
-                <h2 className="text-3xl mt-8 mb-44" >説明文</h2>
+                <h2 className="text-3xl mt-8" >About</h2>
+            </div>
+
+            <div align="center" className="mt-20">
+                <div className="text-2xl">作った経緯</div>
+                <div className="text-1xl border-l-8 border-purple inline-block rounded bg-thin-purple px-6 pt-4">ああああ</div>
+            </div>
+            <div align="center" className="mt-20">
+                <div className="text-2xl">Skills</div>
+                <div className="text-1xl border-l-8 border-purple inline-block rounded bg-thin-purple px-6 pt-4">ああああ</div>
+            </div>
+
+            <div align="center" className="mt-20">
+                <div className="flex justify-center">
+                    <svg className="h-8 w-8 text-Doer-purple fill-Doer-purple"  viewBox="0 0 24 24"    stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polygon points="5 3 19 12 5 21 5 3" /></svg>
+                    <div className="text-2xl">バックエンドの人</div>
+                </div>
+            </div>
+
+            <div align="center" className="mt-20">
+                <div className="flex justify-center">
+                    <svg className="h-8 w-8 text-Doer-purple fill-Doer-purple"  viewBox="0 0 24 24"    stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polygon points="5 3 19 12 5 21 5 3" /></svg>
+                    <div className="text-2xl">フロントエンドの人</div>
+                </div>
             </div>
         </div>
     )
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         'thick-purple': '#A463A9',
         'grey': '#B4B4B4',
         'thick-grey': '#929292',
+        'Doer-purple': '#7A1E94',
       }
     },
   },
