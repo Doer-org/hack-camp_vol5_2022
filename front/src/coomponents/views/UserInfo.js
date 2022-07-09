a43e2f08056578eb2f0575c6161f19b3bbd57fa5
Kouta-fd <kotafd0124@icloud.com>
2022-07-10 05:32:27 +0900

    feat:参加者情報一覧

--- front/src/coomponents/views/UserInfo.js
+++ front/src/coomponents/views/UserInfo.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import UserInfoCard from "../templates/UserInfoCard";
-import { userInfo } from "../../data/userInfo";
+// import { userInfo } from "../../data/userInfo";
+import axios from "axios";
+// import { useLocation } from "react-router-dom";
 
 //userInfo
 // name
@@ -8,9 +10,42 @@ import { userInfo } from "../../data/userInfo";
 // twitter
 
 const UserInfo = () => {
-  const count = userInfo.length;
-  const roomName = "テストテスト";
-  const date = "2022/07/09"
+  // query paramの取得
+  // const search = useLocation().search;
+  // const query = new URLSearchParams(search);
+  // const roomId = query.get("room");
+
+  const roomId =
+    "46e96c86ab6db490de247c05b95a905e19eee8985f341856cb162b1e84d73241";
+  const [roomInfo, setRoomInfo] = useState("");
+  axios
+    .get(`https://go-server-doer-vol5.herokuapp.com/room/${roomId}`)
+    .then((response) => {
+      console.log(response.data);
+      setRoomInfo(response.data.status);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  const [users, setUsers] = useState([]);
+  axios
+    .get(`https://go-server-doer-vol5.herokuapp.com/member/all?room=${roomId}`)
+    .then((response) => {
+      console.log(response.data);
+      setUsers(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  const count = users.length;
+  const roomName = roomInfo.name;
+  const str = roomInfo.created_at;
+  function toDate(str, delim) {
+    const arr = str.split(delim);
+    return new Date(arr[0], arr[1] - 1, arr[2]);
+  }
+  const date = toDate(str, "-");
   return (
     <div className="w-10/12 mx-auto">
       <h1 className="text-3xl text-center mt-10 mb-5">{roomName}</h1>
@@ -22,7 +57,7 @@ const UserInfo = () => {
         <div className="text-[0.8rem]">{date}</div>
       </div>
       <ul>
-        {userInfo.map((user) => {
+        {users.map((user) => {
           return (
             <li>
               <UserInfoCard user={user} />
