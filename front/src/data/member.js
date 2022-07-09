e28b45c2da0d918b9670118d38a3fdbf55d7f887
YagiAyana <cgjd0310@mail4.doshisha.ac.jp>
2022-07-10 01:08:14 +0900

    メンバー情報追加

---
+++ front/src/data/member.js
@@ -0,0 +1,22 @@
+export const member = [
+    {
+        name: "mahiro72（Backend）",
+        github: "mahiro72",
+        twitter: "sino0042900",
+    },
+    {
+        name: "hikari-8（Backend）",
+        github: "hikari-8",
+        twitter: "hikari_pint",
+    },
+    {
+        name: "kouta-fd（Frontend）",
+        github: "kouta-fd",
+        twitter: "KOUTA_FD",
+    },
+    {
+        name: "Yaaagi",
+        github: "Yaaagi",
+        twitter: "yagiyagioyagi",
+    },
+];
