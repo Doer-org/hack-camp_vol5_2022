a82a69cb87af3c7705b365a02df8705fb147e3d0
kazuyan <komokazu.nankai@gmail.com>
2022-08-25 20:34:41 +0900

    fix

--- front/src/components/layout/AppContainer.tsx
+++ front/src/components/layout/AppContainer.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 export const AppContainer: FC<IProps> = ({ children }) => {
   return (
-    <div className="mx-auto max-w-7xl p-6 lg:p-20">
+    <div className="mx-auto max-w-7xl px-6 lg:px-20">
       {children}
     </div>
   )
