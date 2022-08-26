7e2dc178468e28ec0de17fc305eb70b1827b9e18
kazuyan <komokazu.nankai@gmail.com>
2022-08-26 19:20:36 +0900

    remove: strict mode

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,9 @@
 import ReactDOM from 'react-dom/client'
-// import App from "./App";
 import './index.css'
 import { Routing } from './Routing'
-import { StrictMode } from 'react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <div className="text-gray-700">
-      <Routing />
-    </div>
-  </StrictMode>
+  <div className="text-gray-700">
+    <Routing />
+  </div>
 )
