803137c73d5c664b541af3e77fd52c2ee0189950
hikari <kanbarusuruga0127@gmail.com>
2022-08-28 06:20:04 +0900

    fix component at step0

--- front/src/pages/step0/index.tsx
+++ front/src/pages/step0/index.tsx
@@ -1,17 +1,12 @@
 import { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { SecCard } from './SecCard'
+import {RegisterBackground} from './RegisterBackground'
 
 
 export const StepZero: FC = () => {
-    const navigate = useNavigate()
-
     return(
-    <div className="flex h-screen justify-center
-                    bg-thin-purple 
-                    py-10 px-2 ">
+    <RegisterBackground>
     <SecCard />
-        
-    </div>
+    </RegisterBackground>
     )
 }
