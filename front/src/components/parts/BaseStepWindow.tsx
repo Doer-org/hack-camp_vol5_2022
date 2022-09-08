f061277ce9c692ac521eb3c524a570cf28033598
kazuyan <komokazu.nankai@gmail.com>
2022-09-08 23:22:37 +0900

    fix: UI

--- front/src/components/parts/BaseStepWindow.tsx
+++ front/src/components/parts/BaseStepWindow.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 export const BaseStepWindow: FC<IProps> = ({ children }) => {
   return(
-    <div className="min-h-screen rounded bg-thin-purple p-10 py-28 text-gray-600 shadow shadow-doer-purple lg:h-auto lg:w-1/3 lg:bg-white lg:p-10">
+    <div className="min-h-screen rounded bg-thin-purple p-10 py-28 text-gray-600 shadow shadow-doer-purple lg:h-auto lg:min-h-max lg:w-1/3 lg:bg-white lg:p-10">
       {children}
     </div>
   )
