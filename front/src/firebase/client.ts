0e2f77caf998cd5eb60fa44be1a03f0682463f15
kazuyan <komokazu.nankai@gmail.com>
2022-09-09 00:19:49 +0900

    add: firebase

---
+++ front/src/firebase/client.ts
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat"
+import initializeApp = firebase.initializeApp
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID
+}
+
+export const FB = initializeApp(firebaseConfig)
