4c48bb23ce464b52a0a3aec8921c231556277f3c
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2022-09-10 23:49:42 +0900

    fix

--- front/src/firebase/authGithub.ts
+++ front/src/firebase/authGithub.ts
@@ -1,18 +1,19 @@
 import { GithubAuthProvider } from 'firebase/auth'
 import { FB } from '@/firebase/client'
 import * as TE from 'fp-ts/TaskEither'
-export type UserId = string
-export const login = () : TE.TaskEither<Error, UserId> => {  
+
+
+export const login = () : TE.TaskEither<Error, firebase.default.User> => {  
   const auth = FB.auth() 
   const provider =  new GithubAuthProvider()  
   return TE.tryCatch(
     async () => {  
-      const cred = await auth.signInWithPopup(provider)
-      const uid =  cred.user?.uid
-      if (typeof uid === "undefined") {
-        throw Error("uidを取得できませんでした．")
-      }
-      return uid
+      const cred = await auth.signInWithPopup(provider) 
+      if (cred.user === null) {
+        throw Error("uidを取得できませんでした．") 
+      } else {
+        return cred.user 
+      } 
     },
     (e) => {   
       return Error ("ログインに失敗しました．") 
--- front/src/hooks/useFirebase.ts
+++ front/src/hooks/useFirebase.ts
@@ -3,12 +3,12 @@ import * as AG from '@/firebase/authGithub'
 import * as E from 'fp-ts/Either' 
 
 interface IApis {
-	githubLogin: () => Promise<AG.UserId>
+	githubLogin: () => Promise<firebase.default.User>
 	githubLogout: () => Promise<void>
 }
 
 export const useFirebase = (): IApis => { 
-  const githubLogin = async () : Promise<AG.UserId> => {  
+  const githubLogin = async () : Promise<firebase.default.User> => {  
     return await AG.login()().then((ret) => { 
       if (E.isLeft(ret)) {
         throw ret.left
