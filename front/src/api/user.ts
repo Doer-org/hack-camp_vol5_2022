fa00ec02043505fcd9c57fe90f79ebab3ad73b18
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2022-09-16 02:01:04 +0900

    add: ユーザ情報の取得

--- front/src/api/user.ts
+++ front/src/api/user.ts
@@ -2,6 +2,7 @@ import { IApiError } from "@/types/api/ApiError"
 import { AxiosClient } from "@/api/client"
 import { IPostLoginWithGithubInput, IPostLoginWithGithubOutput } from "@/types/api/user"
 import { TaskEither, tryCatch } from "fp-ts/TaskEither"
+import * as TE from 'fp-ts/TaskEither'
 
 // Github ログイン
 export const PostLoginWithGithub = (input: IPostLoginWithGithubInput): TaskEither<IApiError, IPostLoginWithGithubOutput> => {
@@ -18,7 +19,18 @@ export const PostLoginWithGithub = (input: IPostLoginWithGithubInput): TaskEithe
 }
 
 // TODO @aoki GET /user/:uid ユーザの情報を取ってくる
-
+export const GetUserInfo = (userID : string) : TaskEither<IApiError, IPostLoginWithGithubOutput> => {
+  return TE.tryCatch(
+    async () => { 
+      const { data } = await AxiosClient().get(`/user/${userID}`)
+      return data
+    },
+    (e: any) => {
+      const resp: IApiError = { error: e.response.data.error }
+      return resp
+    }
+  )
+}
 // output
 // type User struct {
 //   Uid     string `json:"uid" gorm:"unique"`
